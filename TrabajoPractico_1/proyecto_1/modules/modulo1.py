432672d28ec3c6409a4f1e47f52d54acaa227965
Katrina-scev <katrinascev@gmail.com>
2025-03-26 17:33:58 -0300

    Update modulo1.py

--- TrabajoPractico_1/proyecto_1/modules/modulo1.py
+++ TrabajoPractico_1/proyecto_1/modules/modulo1.py
@@ -1,8 +1,2 @@
-# módulo para organizar funciones o clases utilizadas en nuestro proyecto
-# Crear tantos módulos como sea necesario para organizar el código
-print("hola mundo")
-print("hola hola")
-print ("actualizado")
-print ("hola2")
 # status-add-status-commit -m""-push
-# prueba
+
